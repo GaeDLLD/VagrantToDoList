609ef1430dbc244b03f5bd99128dbbc77be98e2e
Gaëtan <serial.crossfire@gmail.com>
2021-04-19 16:25:07 +0200

    Design done

--- todolist-app/src/app/app.component.ts
+++ todolist-app/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface Task {
+  title: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist-app';
+  tasks: Task[] = [
+    {
+      title: 'Task 1',
+      description: 'Description 1',
+    },
+    {
+      title: 'Task 2',
+      description: 'Description 2',
+    },
+    {
+      title: 'Task 3',
+      description: 'Description 3',
+    },
+  ];
+
+  newTitle: string;
+  newDescription: string;
 }
--- todolist-app/src/app/app.module.ts
+++ todolist-app/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatLineModule} from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatLineModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
