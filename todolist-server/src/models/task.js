664668a4adb70b2bb10c98e5b9ff4ea5ef5d7ea5
maximeBoucherAguilar <maxime.boucheraguilar@gmail.com>
2021-04-19 15:04:02 +0200

    Update todolist model

--- todolist-server/src/models/task.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var TaskSchema = new mongoose.Schema({
-    title: {type: String, lowercase: true, required: [true, "can't be blank"] },
-    description: string
-}, {timestamps: true});
-
-mongoose.model('Task', TaskSchema);
