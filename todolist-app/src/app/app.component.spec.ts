0e76587dd2312cf0f63b7ed72aee624f06984ced
Gaëtan <serial.crossfire@gmail.com>
2021-04-26 13:38:44 +0200

    Add karma-junit-reporter

--- todolist-app/src/app/app.component.spec.ts
+++ todolist-app/src/app/app.component.spec.ts
@@ -16,16 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todolist-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todolist-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todolist-app app is running!');
-  });
 });
